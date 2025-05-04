b701e6020e67207b220709a1a34c7f5287dfc7bf
Ridho Ramidianto <ridho.ramadianto@bni.co.id>
2025-05-05 02:41:54 +0700

    Fix: make open_keypad more flexible

--- pages/dialer_page.py
+++ pages/dialer_page.py
@@ -4,9 +4,20 @@ class DialerPage:
     def __init__(self, driver):
         self.driver = driver
 
-    def open_keypad(self):
+def open_keypad(self):
+    try:
+        print("🟢 Mencoba klik 'Phone'")
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "Phone").click()
+        time.sleep(2)
+    except Exception as e:
+        print("ℹ️ Tidak bisa klik Phone (mungkin sudah dibuka)")
+
+    try:
+        print("🟢 Mencoba klik 'key pad'")
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "key pad").click()
+        time.sleep(1)
+    except Exception as e:
+        print("❗ Tidak bisa klik key pad:", e)
 
     def enter_digits(self):
         self.driver.find_element(AppiumBy.ACCESSIBILITY_ID, "1,").click()
